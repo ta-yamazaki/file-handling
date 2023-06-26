d1b797595aa5e183d25b90211bfce76d543b2ea5
takashi.yamazaki <takashi.yamazaki@aideal.world>
2023-06-26 11:36:25 +0900

    セルのスタイル追加

--- src/main/java/file/handling/application/service/report/XlsxReport.java
+++ src/main/java/file/handling/application/service/report/XlsxReport.java
@@ -4,10 +4,7 @@ import file.handling.domain.model.product.Product;
 import file.handling.domain.model.product.Products;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.springframework.web.servlet.view.document.AbstractXlsxView;
 
 import java.net.URLEncoder;
@@ -16,44 +13,90 @@ import java.util.Map;
 
 public class XlsxReport extends AbstractXlsxView {
 
+    private Workbook workbook;
+    private Sheet sheet;
+
     @Override
     protected void buildExcelDocument(Map<String, Object> model,
                                       Workbook workbook,
                                       HttpServletRequest request,
                                       HttpServletResponse response) {
+        this.workbook = workbook;
+
         String fileName = "商品一覧.xlsx";
+        this.sheet = workbook.createSheet(fileName);
+
+        // 行固定
+        sheet.createFreezePane(0, 1);
+
         response.setHeader("Content-Disposition", "attachment; filename*=UTF-8''" + URLEncoder.encode(fileName, StandardCharsets.UTF_8));
 
-        Sheet sheet = workbook.createSheet(fileName);
-        // 枠線の非表示
-//        sheet.setDisplayGridlines(false);
-        setHeader(sheet);
+        setHeader();
 
         Products products = (Products) model.get("products");
-        setBody(sheet, products);
+        setBody(products);
+
+        // 枠線の非表示
+//        sheet.setDisplayGridlines(false);
+
+        setColumnsWidth();
     }
 
-    private void setHeader(Sheet sheet) {
-        Row headerRow = sheet.createRow(0);
+    private void setHeader() {
+        Row headerRow = this.sheet.createRow(0);
 
-        writeCell(headerRow, "商品番号");
-        writeCell(headerRow, "商品名");
-        writeCell(headerRow, "商品画像ファイル名");
+        writeHeaderCell(headerRow, "商品番号");
+        writeHeaderCell(headerRow, "商品名");
+        writeHeaderCell(headerRow, "商品画像ファイル名");
     }
 
-    private void setBody(Sheet sheet, Products products) {
+    private void setBody(Products products) {
         for (Product product : products.list()) {
-            Row row = sheet.createRow(sheet.getLastRowNum() + 1);
+            int lastRow = this.sheet.getLastRowNum();
+            Row row = this.sheet.createRow( lastRow + 1);
 
-            writeCell(row, product.number()); // 商品番号
-            writeCell(row, product.name()); // 商品名
-            writeCell(row, product.fileName()); // 商品画像ファイル名
+            writeBodyCell(row, product.number()); // 商品番号
+            writeBodyCell(row, product.name()); // 商品名
+            writeBodyCell(row, product.fileName()); // 商品画像ファイル名
         }
     }
 
-    private void writeCell(Row row, String value) {
+    private void writeHeaderCell(Row row, String value) {
         int lastCellNum = row.getLastCellNum();
         Cell cell = row.createCell(lastCellNum == -1 ? 0 : lastCellNum);
         cell.setCellValue(value);
+        cell.setCellStyle(headerCellStyle());
+    }
+
+    private void writeBodyCell(Row row, String value) {
+        int lastCellNum = row.getLastCellNum();
+        Cell cell = row.createCell(lastCellNum == -1 ? 0 : lastCellNum);
+        cell.setCellValue(value);
+        cell.setCellStyle(bodyCellStyle());
+    }
+
+    private CellStyle headerCellStyle() {
+        // 日本語での列幅の自動調整のため、フォントを明示的に指定する。
+        CellStyle cellStyle = this.workbook.createCellStyle();
+        Font font = this.workbook.createFont();
+        font.setFontName("游ゴシック");
+        font.setBold(true);
+        cellStyle.setFont(font);
+        return cellStyle;
+    }
+
+    private CellStyle bodyCellStyle() {
+        // 日本語での列幅の自動調整のため、フォントを明示的に指定する。
+        CellStyle cellStyle = this.workbook.createCellStyle();
+        Font font = this.workbook.createFont();
+        font.setFontName("游ゴシック");
+        cellStyle.setFont(font);
+        return cellStyle;
+    }
+
+    private void setColumnsWidth() {
+        this.sheet.autoSizeColumn(0);  // 商品番号
+        this.sheet.autoSizeColumn(1);  // 商品名
+        this.sheet.autoSizeColumn(2);  // 商品画像ファイル名
     }
 }
